5cea5a0184504186eef735b3aec74a0f237726ff
Huong <huongnguyen.nth1709@gmail.com>
2025-02-14 14:33:54 +0700

    build footer UI for auth layout

--- src/layouts/AuthLayout/Footer.jsx
+++ src/layouts/AuthLayout/Footer.jsx
@@ -1,5 +1,26 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className='fixed bottom-0 inset-x-0 border-t border-solid border-gray-300'>
+      <ul className='text-sm flex justify-around text-gray-500 items-center w-inner h-[60px] m-auto'>
+        <li>
+          <a href='' className='footer-link'>
+            Help Center
+          </a>
+        </li>
+        <li>
+          <a href='' className='footer-link'>
+            Terms of Service
+          </a>
+        </li>
+        <li>
+          <a href='' className='footer-link'>
+            Privacy Policy
+          </a>
+        </li>
+        <li>&copy;2025chloehuongnth. All rights reserved.</li>
+      </ul>
+    </footer>
+  );
 };
 
 export default Footer;
