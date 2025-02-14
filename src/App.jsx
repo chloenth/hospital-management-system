3fb9071fdd2f7e0e41c2dfb981e57ad2fa43ce59
Huong <huongnguyen.nth1709@gmail.com>
2025-02-14 11:46:26 +0700

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className='header'>Hello, Tailwind CSS in React</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+// Convert `import.meta.url` to a directory path
+const __filename = fileURLToPath(import.meta.url); // Get current file path
+const __dirname = dirname(__filename); // Get current directory
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'src'),
+    },
+  },
+});
